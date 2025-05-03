e16ec700a4fb8296a79a33a7e5fdbae9dcc50d43
bhmberkan <berkanburakturgut@gmail.com>
2025-05-03 13:39:53 +0300

    Geçici commit: Rebase öncesi

--- lib/constants/colors.dart
+++
@@ -1,9 +0,0 @@
-
-class AppColors{
-
-static const String primaryColor = "#F8F4E1";
-static const String secondaryColor = "#F8F4E1";
-static const String backgroundColor ="#74512D";
-static const String sonColor = "#4E1F00";
-}
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dersleri/constants/colors.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:hexcolor/hexcolor.dart';
-
 
 void main() {
    // firebase gibi şeyler
@@ -22,36 +18,17 @@ class _WidgetDetailState extends State<WidgetDetail> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(textTheme: GoogleFonts.acmeTextTheme()),
       home: Scaffold(
         appBar: AppBar(
           title: const Text("widget detail"), // bir widgette değişken kullanadıysak const atıyoruz
           centerTitle: true,
           backgroundColor: Colors.deepOrange,
-          leading:  Text("menü", 
-          style: TextStyle(
-            color: HexColor(AppColors.primaryColor
-            ),
-            ),
-            ), // menu ikonlarının geldiği yer 
+          leading: const Text("menü"), // menu ikonlarının geldiği yer 
           // actions: [], // menü gibi sağ üstte bulunan sekme liste alır.
         ),
         body: // Text("Hello flutter"), // sol üste gelecek ortalamak istersek
          Center(
-          child: Column(
-            mainAxisAlignment:  MainAxisAlignment.center,
-            children: [
-              Text(value.toString()),
-              Text("Flutter ögrenmek", 
-             style: GoogleFonts.acme(
-              fontSize: 40,
-              color: HexColor(AppColors.backgroundColor)
-            
-             )
-             ),
-             const Text("örnek")
-            ],
-          ),
+          child: Text(value.toString()), // ortalamak istersek
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
