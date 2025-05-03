484a6f95779ff23966a7d4283542099f78c0b1a8
bhmberkan <berkanburakturgut@gmail.com>
2025-05-03 13:12:53 +0300

    firs commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dersleri/constants/colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
+
 
 void main() {
    // firebase gibi şeyler
@@ -18,17 +22,36 @@ class _WidgetDetailState extends State<WidgetDetail> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(textTheme: GoogleFonts.acmeTextTheme()),
       home: Scaffold(
         appBar: AppBar(
           title: const Text("widget detail"), // bir widgette değişken kullanadıysak const atıyoruz
           centerTitle: true,
           backgroundColor: Colors.deepOrange,
-          leading: const Text("menü"), // menu ikonlarının geldiği yer 
+          leading:  Text("menü", 
+          style: TextStyle(
+            color: HexColor(AppColors.primaryColor
+            ),
+            ),
+            ), // menu ikonlarının geldiği yer 
           // actions: [], // menü gibi sağ üstte bulunan sekme liste alır.
         ),
         body: // Text("Hello flutter"), // sol üste gelecek ortalamak istersek
          Center(
-          child: Text(value.toString()), // ortalamak istersek
+          child: Column(
+            mainAxisAlignment:  MainAxisAlignment.center,
+            children: [
+              Text(value.toString()),
+              Text("Flutter ögrenmek", 
+             style: GoogleFonts.acme(
+              fontSize: 40,
+              color: HexColor(AppColors.backgroundColor)
+            
+             )
+             ),
+             const Text("örnek")
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
