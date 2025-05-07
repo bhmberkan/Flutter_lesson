613996f065fd48fc26825528a0c4a0c640bc8461
bhmberkan <berkanburakturgut@gmail.com>
2025-05-07 13:09:11 +0300

    Geçici commit: FlutterLesson13 rebase öncesi

--- lib/main.dart
+++ lib/main.dart
@@ -23,35 +23,24 @@ class _WidgetDetailState extends State<WidgetDetail> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(textTheme: GoogleFonts.acmeTextTheme()),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("widget detail"), // bir widgette değişken kullanadıysak const atıyoruz
-          centerTitle: true,
-          backgroundColor: Colors.deepOrange,
-          leading:  Text("menü", 
-          style: TextStyle(
-            color: HexColor(AppColors.primaryColor
-            ),
-            ),
-            ), // menu ikonlarının geldiği yer 
-          // actions: [], // menü gibi sağ üstte bulunan sekme liste alır.
-        ),
-        body: Container(
-         // padding: const EdgeInsets.symmetric(vertical: 20.0,horizontal: 40.0), //only(left: 20.0, top: 10.0),  //fromLTRB(40, 5, 20, 10), //EdgeInsets.all(20.0),
-          margin: const EdgeInsets.all(20.2),
-          color: Colors.green,
-          child: const Text("örnek"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              value = value + 1;
-            });
-          },
-          child: const Text("+"),
-        ),
-      ),
+      home: Row(
+        children: [
+          Flexible( 
+            flex: 2,
+            child: Container(
+              color: Colors.amberAccent,
+              child: Text("hello asdasdada"),
+              ),
+          ),
+          Flexible(
+            child: Container(
+                color: Colors.white,
+                child: Text("World"),
+              ),
+          )
+        ],
+      )
     );
   }
-}
+} 
 
